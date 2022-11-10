09b783d1ce8a14931c30ceea95a012c8106ffcf4
Taehyup Ko <kth3447@gmail.com>
2022-11-10 15:16:23 +0900

    tutorial - Spring Boot with Kotlin Coroutines and RSocket : part 1~3

---
+++ kotlin-spring-chat/kotlin-spring-chat/src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin.chat.service
+
+import com.example.kotlin.chat.asDomainObject
+import com.example.kotlin.chat.asViewModel
+import com.example.kotlin.chat.mapToViewModel
+import com.example.kotlin.chat.repository.ContentType
+import com.example.kotlin.chat.repository.Message
+import com.example.kotlin.chat.repository.MessageRepository
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Service
+import java.net.URL
+
+@Service
+@Primary
+class PersistentMessageService(val messageRepository: MessageRepository): MessageService {
+
+    override fun latest(): List<MessageVM> =
+        messageRepository.findLatest().mapToViewModel()
+
+    override fun after(messageId: String): List<MessageVM> =
+        messageRepository.findLatest(messageId).mapToViewModel()
+
+    override fun post(message: MessageVM) {
+        messageRepository.save(message.asDomainObject())
+    }
+}
